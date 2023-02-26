69afc304d88a2b7cb0ef47c5419a54d4277928f4
rawphl <ivancammarota@gmail.com>
2023-02-26 20:45:30 +0100

    Eyecandy and session initialization fix

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,18 +2,23 @@ import Head from "next/head"
 import styles from "./dashboard.module.css"
 
 export default function Dashboard({ posts }) {
-  console.log({ posts })
   return (
     <>
       <Head>
         <title>Dashboard</title>
       </Head>
+
       <main className={styles.dashboard}>
-        <h1>Dashboard</h1>
+        <div>
+          <h1>Dashboard</h1>
+          <h2>nextjsapp</h2>
+        </div>
+
         {
           posts.map(post => (
-            <article key={post.id}>
+            <article key={post.id} className={styles.post}>
               <h2>{post.title}</h2>
+              <img src="https://via.placeholder.com/398x250" width={398} height={250} />
               <p>{post.body}</p>
             </article>
           ))
