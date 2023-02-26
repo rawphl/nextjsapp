723b27929efd20e22816f76b27119d7870b1fe37
Ivan Cammarota <ivancammarota@gmail.com>
2023-02-27 00:49:56 +0100

    Format

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,16 +1,23 @@
 import { fetchPosts } from "@/lib/services/posts"
 import Head from "next/head"
-import { useQuery } from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query"
 import styles from "./dashboard.module.css"
 import Image from "next/image"
 
 function usePosts({ initialData }) {
-  return useQuery({ queryKey: ["posts"], queryFn: fetchPosts, initialData, cacheTime: 10000})
+  return useQuery(
+    ["posts"],
+    fetchPosts, 
+    {
+      staleTime: 1000 * 60 * 60,
+      initialData
+    }
+  )
 }
 
-export default function Dashboard({ posts }) {
-  const { data, error } = usePosts({ initialData: posts })
-  if (!data) return <div>Loading</div>
+export default function Dashboard(props) {
+  const { data: posts, error } = usePosts({ initialData: props.posts })
+  if (!posts) return <div>Loading</div>
   if (error) return <div>{`An error has occurred: ${error.message}`}</div>
   return (
     <>
@@ -25,10 +32,16 @@ export default function Dashboard({ posts }) {
         </div>
 
         {
-          data?.map(post => (
+          posts.map(post => (
             <article key={post.id} className={styles.post}>
               <h2>{post.title}</h2>
-              <Image src="https://via.placeholder.com/398x250" width={398} height={250} priority alt={post.id + "-img"}/>
+              <Image
+                src="https://via.placeholder.com/398x250"
+                alt={`${post.id}-image`}
+                width={398}
+                height={250}
+                priority
+              />
               <p>{post.body}</p>
             </article>
           ))
