a75e0e2e1c4bfc4d5d738648c22b549e69c21a5a
Ivan Cammarota <cammi@bbcag.ch>
2023-02-28 11:40:21 +0100

    Add link to dashboard on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head"
+import Link from "next/link"
 import styles from "./index.module.css"
 
-export default function Index() {
+export default function Index({ session }) {
   return (
     <>
       <Head>
@@ -9,6 +10,7 @@ export default function Index() {
       </Head>
       <main className={styles.index}>
         <h1>Index</h1>
+        {session && <Link href="/dashboard">Dashboard</Link>}
       </main>
     </>
   )
